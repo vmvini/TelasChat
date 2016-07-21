2a48af16789b97733bbd4b58b253a167b216bb16
Marcus Vinicius <vmvini@hotmail.com>
2016-07-21 04:53:09 -0300

    nine patched bubble chat images

---
+++ app/src/main/java/io/github/vmvini/exerciciopdm/ThirdActivity.java
@@ -0,0 +1,16 @@
+package io.github.vmvini.exerciciopdm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vmvini on 21/07/16.
+ */
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thirdactivity);
+    }
+}
